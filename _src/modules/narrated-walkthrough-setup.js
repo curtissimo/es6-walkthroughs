98536eb7b08eed10ea43c2efb827f07fba7ae7db
Curtis Schlak <realistschuckle@gmail.com>
2014-11-25 23:52:31 -0600

    Block insertion of text now working.

--- _src/modules/narrated-walkthrough-setup.js
+++ _src/modules/narrated-walkthrough-setup.js
@@ -1,14 +1,151 @@
 import loader from 'loader';
 
+const HAVE_ENOUGH_DATA = 4;
+
+let keyframes = {};
+let keystops = [ 0 ];
+let lowerTime = 0;
+let upperTime = 0;
+let upperTimeIndex = 0;
+
 let handlers = {
+  markerChanged(e) {
+    let narration = document.getElementById('narration');
+    let time = document.querySelector('#play .time');
+    let totalSeconds = narration.currentTime;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = Math.floor(totalSeconds) % 60;
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    time.innerHTML = minutes + ":" + seconds;
+
+    if (totalSeconds < upperTime && totalSeconds >= lowerTime) {
+      switch(keyframes[lowerTime].position) {
+        case -1:
+          editor.navigateFileStart();
+          break;
+        case 1:
+          editor.navigateFileEnd();
+          break;
+        case 0:
+          editor.setValue('');
+          break;
+      }
+      editor.insert(keyframes[lowerTime].text);
+      editor.clearSelection();
+      lowerTime = upperTime;
+      upperTime = keystops[upperTimeIndex];
+      upperTimeIndex += 1;
+    }
+  },
+  pageReady() {
+    let rewindButton = document.getElementById('rewind');
+    rewindButton.addEventListener('click', handlers.rewind);
+    rewindButton.disabled = false;
+
+    let playButton = document.getElementById('play');
+    playButton.disabled = false;
+    playButton.addEventListener('click', handlers.togglePlay);
+
+    let narration = document.getElementById('narration');
+    narration.addEventListener('playing', handlers.played);
+    narration.addEventListener('pause', handlers.paused);
+    narration.addEventListener('timeupdate', handlers.markerChanged);
+
+    setTimeout(narration.play.bind(narration), 500);
+
+    handlers.resize();
+
+    if (keystops.length > 2) {
+      lowerTime = keystops[upperTimeIndex];
+      upperTimeIndex += 1;
+
+      upperTime = keystops[upperTimeIndex];
+      upperTimeIndex += 1;
+    }
+  },
+  paused() {
+    let icon = document.querySelector('#play .fa');
+    icon.className = 'fa fa-play';
+
+    let label = document.querySelector('#play .label');
+    label.innerHTML = 'Play';
+  },
+  played() {
+    let icon = document.querySelector('#play .fa');
+    icon.className = 'fa fa-pause';
+
+    let label = document.querySelector('#play .label');
+    label.innerHTML = 'Pause';
+  },
   resize() {
-    console.log('moo');
+    let height = window.innerHeight - 2 * document.querySelector('form.unsubmitable').offsetHeight - 10;
+    document.getElementById('console').style.height = height + 'px';
+    editor.container.style.height = height + 20 + 'px';
+    editor.resize();
+  },
+  rewind(e) {
+    e.preventDefault();
+    let narration = document.getElementById('narration');
+    narration.currentTime = 0;
+    editor.setValue('');
+
+    lowerTime = 0;
+    upperTime = 0;
+    upperTimeIndex = 0;
+    if (keystops.length > 2) {
+      lowerTime = keystops[upperTimeIndex];
+      upperTimeIndex += 1;
+
+      upperTime = keystops[upperTimeIndex];
+      upperTimeIndex += 1;
+    }
+  },
+  togglePlay(e) {
+    e.preventDefault();
+    let narration = document.getElementById('narration');
+    if (narration.paused) {
+      narration.play();
+    } else {
+      narration.pause();
+    }
   }
 }
 
-let test = () => document.getElementById('editor').className.contains('ace-twilight');
-let cb = () => handlers.resize();
+let test = () => {
+  return document.getElementById('editor').className.contains('ace-twilight')
+      && document.getElementById('narration').readyState === HAVE_ENOUGH_DATA;
+};
+let cb = handlers.pageReady;
 loader('workspace', 'loader', test, cb);
 
 let editor = ace.edit('editor');
+editor.setReadOnly(true);
 editor.setTheme('ace/theme/twilight');
+editor.getSession().setTabSize(2);
+editor.getSession().setMode('ace/mode/javascript');
+
+window.addEventListener('resize', handlers.resize);
+handlers.resize();
+
+export let install = (kf, keys) => {
+  Object.keys(kf).forEach(k => {
+    if (typeof kf[k] === 'string') {
+      kf[k] = { text: kf[k], position: 1 };
+    }
+    if (kf[k].position === 'end') {
+      kf[k].position = 1;
+    } else if (kf[k].position === 'start') {
+      kf[k].position = -1;
+    } else if (kf[k].position === 'replace') {
+      kf[k].position = 0;
+    }
+    keystops.push(k - 0);
+  });
+  keyframes = kf;
+  keyframes['0'] = { text: '', position: 0 };
+  keyframes['600'] = { text: '', position: -1 };
+  keystops.push(600);
+  keystops.sort((a, b) => a - b);
+};
--- assets/narrated-walkthrough-setup.js
+++ assets/narrated-walkthrough-setup.js
@@ -1,28 +1,168 @@
 System.register("narrated-walkthrough-setup", ["loader"], function($__export) {
   "use strict";
   var __moduleName = "narrated-walkthrough-setup";
+  function require(path) {
+    return $traceurRuntime.require("narrated-walkthrough-setup", path);
+  }
   var loader,
+      HAVE_ENOUGH_DATA,
+      keyframes,
+      keystops,
+      lowerTime,
+      upperTime,
+      upperTimeIndex,
       handlers,
       test,
       cb,
-      editor;
+      editor,
+      install;
   return {
     setters: [function(m) {
       loader = m.default;
     }],
     execute: function() {
-      handlers = {resize: function() {
-          console.log('moo');
-        }};
+      HAVE_ENOUGH_DATA = 4;
+      keyframes = {};
+      keystops = [0];
+      lowerTime = 0;
+      upperTime = 0;
+      upperTimeIndex = 0;
+      handlers = {
+        markerChanged: function(e) {
+          var narration = document.getElementById('narration');
+          var time = document.querySelector('#play .time');
+          var totalSeconds = narration.currentTime;
+          var minutes = Math.floor(totalSeconds / 60);
+          var seconds = Math.floor(totalSeconds) % 60;
+          if (seconds < 10) {
+            seconds = "0" + seconds;
+          }
+          time.innerHTML = minutes + ":" + seconds;
+          if (totalSeconds < upperTime && totalSeconds >= lowerTime) {
+            switch (keyframes[lowerTime].position) {
+              case -1:
+                editor.navigateFileStart();
+                break;
+              case 1:
+                editor.navigateFileEnd();
+                break;
+              case 0:
+                editor.setValue('');
+                break;
+            }
+            editor.insert(keyframes[lowerTime].text);
+            editor.clearSelection();
+            lowerTime = upperTime;
+            upperTime = keystops[upperTimeIndex];
+            upperTimeIndex += 1;
+          }
+        },
+        pageReady: function() {
+          var rewindButton = document.getElementById('rewind');
+          rewindButton.addEventListener('click', handlers.rewind);
+          rewindButton.disabled = false;
+          var playButton = document.getElementById('play');
+          playButton.disabled = false;
+          playButton.addEventListener('click', handlers.togglePlay);
+          var narration = document.getElementById('narration');
+          narration.addEventListener('playing', handlers.played);
+          narration.addEventListener('pause', handlers.paused);
+          narration.addEventListener('timeupdate', handlers.markerChanged);
+          setTimeout(narration.play.bind(narration), 500);
+          handlers.resize();
+          if (keystops.length > 2) {
+            lowerTime = keystops[upperTimeIndex];
+            upperTimeIndex += 1;
+            upperTime = keystops[upperTimeIndex];
+            upperTimeIndex += 1;
+          }
+        },
+        paused: function() {
+          var icon = document.querySelector('#play .fa');
+          icon.className = 'fa fa-play';
+          var label = document.querySelector('#play .label');
+          label.innerHTML = 'Play';
+        },
+        played: function() {
+          var icon = document.querySelector('#play .fa');
+          icon.className = 'fa fa-pause';
+          var label = document.querySelector('#play .label');
+          label.innerHTML = 'Pause';
+        },
+        resize: function() {
+          var height = window.innerHeight - 2 * document.querySelector('form.unsubmitable').offsetHeight - 10;
+          document.getElementById('console').style.height = height + 'px';
+          editor.container.style.height = height + 20 + 'px';
+          editor.resize();
+        },
+        rewind: function(e) {
+          e.preventDefault();
+          var narration = document.getElementById('narration');
+          narration.currentTime = 0;
+          editor.setValue('');
+          lowerTime = 0;
+          upperTime = 0;
+          upperTimeIndex = 0;
+          if (keystops.length > 2) {
+            lowerTime = keystops[upperTimeIndex];
+            upperTimeIndex += 1;
+            upperTime = keystops[upperTimeIndex];
+            upperTimeIndex += 1;
+          }
+        },
+        togglePlay: function(e) {
+          e.preventDefault();
+          var narration = document.getElementById('narration');
+          if (narration.paused) {
+            narration.play();
+          } else {
+            narration.pause();
+          }
+        }
+      };
       test = (function() {
-        return document.getElementById('editor').className.contains('ace-twilight');
+        return document.getElementById('editor').className.contains('ace-twilight') && document.getElementById('narration').readyState === HAVE_ENOUGH_DATA;
       });
-      cb = (function() {
-        return handlers.resize();
-      });
+      cb = handlers.pageReady;
       loader('workspace', 'loader', test, cb);
       editor = ace.edit('editor');
+      editor.setReadOnly(true);
       editor.setTheme('ace/theme/twilight');
+      editor.getSession().setTabSize(2);
+      editor.getSession().setMode('ace/mode/javascript');
+      window.addEventListener('resize', handlers.resize);
+      handlers.resize();
+      install = $__export("install", (function(kf, keys) {
+        Object.keys(kf).forEach((function(k) {
+          if (typeof kf[k] === 'string') {
+            kf[k] = {
+              text: kf[k],
+              position: 1
+            };
+          }
+          if (kf[k].position === 'end') {
+            kf[k].position = 1;
+          } else if (kf[k].position === 'start') {
+            kf[k].position = -1;
+          } else if (kf[k].position === 'replace') {
+            kf[k].position = 0;
+          }
+          keystops.push(k - 0);
+        }));
+        keyframes = kf;
+        keyframes['0'] = {
+          text: '',
+          position: 0
+        };
+        keyframes['600'] = {
+          text: '',
+          position: -1
+        };
+        keystops.push(600);
+        keystops.sort((function(a, b) {
+          return a - b;
+        }));
+      }));
     }
   };
 });
